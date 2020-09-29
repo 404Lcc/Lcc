e66fdc1daf22d722c498aefcbcd1fa95f86ca8b6
Lccxy <1097304582@qq.com>
2020-09-29 23:42:06 +0800

    修改打包 自动化更换dll

--- Assets/Editor/Tool/Preprocess.cs
+++ Assets/Editor/Tool/Preprocess.cs
@@ -9,6 +9,22 @@ public class Preprocess : IPreprocessBuildWithReport, IPostprocessBuildWithRepor
     public int callbackOrder => 0;
     public void OnPreprocessBuild(BuildReport report)
     {
+#if Release
+        if (File.Exists("Assets/Resources/Text/Unity.Hotfix.dll.bytes"))
+        {
+            File.Delete("Assets/Resources/Text/Unity.Hotfix.dll.bytes");
+        }
+        if (File.Exists("Assets/Resources/Text/Unity.Hotfix.pdb.bytes"))
+        {
+            File.Delete("Assets/Resources/Text/Unity.Hotfix.pdb.bytes");
+        }
+        if (File.Exists("Temp/bin/Release/Unity.Hotfix.dll"))
+        {
+            File.Copy("Temp/bin/Release/Unity.Hotfix.dll", "Assets/Resources/Text/Unity.Hotfix.dll.bytes", true);
+            GameUtil.SaveAsset("Assets/Resources/Text/Unity.Hotfix.dll.bytes", GameUtil.RijndaelEncrypt("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx", GameUtil.GetAsset("Assets/Resources/Text/Unity.Hotfix.dll.bytes")));
+            AssetDatabase.Refresh();
+        }
+#endif
         if (File.Exists("Assets/Hotfix/Unity.Hotfix.asmdef"))
         {
             File.Move("Assets/Hotfix/Unity.Hotfix.asmdef", "Assets/Hotfix/Unity.Hotfix.asmdef~");
