c9981f0be301c075e9d1dda1b3cffe9a68d8ac7a
Lccxy <1097304582@qq.com>
2020-10-11 18:44:59 +0800

    补充可视化对象类型

---
+++ Assets/Editor/ObjectType/ObjectType/Vector2IntObjectType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+[ObjectType(typeof(Vector2Int))]
+public class Vector2IntObjectType : IObjectType
+{
+    public object Draw(Type type, string name, object value)
+    {
+        return EditorGUILayout.Vector2IntField(name, (Vector2Int)value);
+    }
+}
---
+++ Assets/Editor/ObjectType/ObjectType/Vector3IntObjectType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+
+[ObjectType(typeof(Vector3Int))]
+public class Vector3IntObjectType : IObjectType
+{
+    public object Draw(Type type, string name, object value)
+    {
+        return EditorGUILayout.Vector3IntField(name, (Vector3Int)value);
+    }
+}
