bf2be6afe2e05b59dd0942e1a16154a4754f0371
Lccxy <1097304582@qq.com>
2022-03-07 23:44:12 +0800

    增加扩展方法

---
+++ Assets/Scripts/Runtime/Core/Expand/Vector3/Vector3Expand.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace LccModel
+{
+    public static class Vector3Expand
+    {
+        public static Vector3 ScreenToUGUI(this Vector3 screenPoint, RectTransform rect)
+        {
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(rect, screenPoint, null, out Vector2 localPosition);
+            return localPosition;
+        }
+    }
+}
