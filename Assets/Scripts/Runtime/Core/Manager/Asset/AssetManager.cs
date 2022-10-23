b7bb935c482a2e9bd0f795842b260d50d2b2f973
Lccxy <1097304582@qq.com>
2022-10-23 17:09:01 +0800

    加一个红点

--- Assets/Scripts/Runtime/Core/Manager/Asset/AssetManager.cs
+++ Assets/Scripts/Runtime/Core/Manager/Asset/AssetManager.cs
@@ -1,4 +1,6 @@
 ﻿using BM;
+using ET;
+using System.Collections.Generic;
 using UnityEngine;
 using Object = UnityEngine.Object;
 
@@ -6,7 +8,8 @@ namespace LccModel
 {
     public class AssetManager : Singleton<AssetManager>
     {
-        private string GetAssetPath(string name, string suffix, params string[] types)
+        private Dictionary<string, LoadHandler> _loadedDict = new Dictionary<string, LoadHandler>();
+        public string GetAssetPath(string name, string suffix, params string[] types)
         {
             if (types.Length == 0) return name;
             string path = string.Empty;
@@ -20,26 +23,71 @@ namespace LccModel
             }
             return $"Assets/Bundles/{path}{suffix}";
         }
-        public LoadHandler LoadAssetAsync<T>(string name, string suffix, params string[] types) where T : Object
+
+
+
+
+
+        public async ETTask<LoadHandler> LoadAssetAsync<T>(string name, string suffix, params string[] types) where T : Object
         {
             string path = GetAssetPath(name, suffix, types);
-            AssetComponent.LoadAsync<GameObject>(out LoadHandler loadHandler, path);
-            return loadHandler;
+            if (_loadedDict.ContainsKey(path))
+            {
+                return _loadedDict[path];
+            }
+            await AssetComponent.LoadAsync<GameObject>(out LoadHandler handler, path);
+            if (!_loadedDict.ContainsKey(path))
+            {
+                _loadedDict.Add(path, handler);
+            }
+            return handler;
+        }
+        public T LoadAsset<T>(out LoadHandler handler, string name, string suffix, params string[] types) where T : Object
+        {
+            string path = GetAssetPath(name, suffix, types);
+            if (_loadedDict.ContainsKey(path))
+            {
+                handler = _loadedDict[path];
+                return (T)handler.Asset;
+            }
+            T asset = AssetComponent.Load<T>(out handler, path);
+            if (!_loadedDict.ContainsKey(path))
+            {
+                _loadedDict.Add(path, handler);
+            }
+            return asset;
         }
         public T LoadAsset<T>(string name, string suffix, params string[] types) where T : Object
         {
-            string path = GetAssetPath(name, suffix, types);
-            return AssetComponent.Load<T>(path);
+            return LoadAsset<T>(out LoadHandler handler, name, suffix, types);
         }
-        public void UnLoadAsset(LoadHandler loadHandler)
+
+
+
+
+
+        public void UnLoadAsset(string name, string suffix, params string[] types)
         {
             //Resources.UnloadAsset仅能释放非GameObject和Component的资源 比如Texture Mesh等真正的资源 对于由Prefab加载出来的Object或Component,则不能通过该函数来进行释放
-            AssetComponent.UnLoad(loadHandler);
+            string path = GetAssetPath(name, suffix, types);
+            if (_loadedDict.ContainsKey(path))
+            {
+                AssetComponent.UnLoad(_loadedDict[path]);
+            }
+
         }
+
+
+
         public void UnLoadAllAssets()
         {
             AssetComponent.UnLoadAllAssets();
         }
+
+
+
+
+
         public GameObject InstantiateAsset(string name, params string[] types)
         {
             GameObject asset = LoadAsset<GameObject>(name, AssetSuffix.Prefab, types);
