45890b7f769b56d50be58429909a51c5cfffe877
Lccxy <1097304582@qq.com>
2023-02-12 18:37:31 +0800

    补充框架工具

--- Assets/Framework/Runtime/Util/Random/RandomUtil.cs
+++ Assets/Framework/Runtime/Util/Random/RandomUtil.cs
@@ -4,18 +4,17 @@ namespace LccModel
 {
     public static class RandomUtil
     {
-        private static Random _random = new Random();
-
+        public static Random random = new Random();
 
         public static int RandomNumber(int lower, int upper)
         {
-            int value = _random.Next(lower, upper);
+            int value = random.Next(lower, upper);
             return value;
         }
 
         public static int RandomRate()
         {
-            int value = _random.Next(1, 101);
+            int value = random.Next(1, 101);
             return value;
         }
     }
