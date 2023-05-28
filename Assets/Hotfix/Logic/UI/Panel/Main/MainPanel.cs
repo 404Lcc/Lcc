cd4653017c491cb715f88350a06de59cd47b1ec0
Lccxy <1097304582@qq.com>
2023-05-29 00:00:38 +0800

    测试代码

--- Assets/Hotfix/Logic/UI/Panel/Main/MainPanel.cs
+++ Assets/Hotfix/Logic/UI/Panel/Main/MainPanel.cs
@@ -1,4 +1,7 @@
-﻿using UnityEngine.UI;
+﻿using LccModel;
+using UnityEngine;
+using UnityEngine.UI;
+using YooAsset;
 
 namespace LccHotfix
 {
@@ -8,6 +11,9 @@ namespace LccHotfix
     public class UIMainPanel : UIPanel<UIMainModel>
     {
         public Button testBtn;
+        public Button test1Btn;
+        public Button test2Btn;
+        public Button test3Btn;
         public override void OnInitComponent(Panel panel)
         {
             base.OnInitComponent(panel);
@@ -29,11 +35,27 @@ namespace LccHotfix
         public override void OnRegisterUIEvent(Panel panel)
         {
             testBtn.onClick.AddListener(OnTest);
+            test1Btn.onClick.AddListener(OnTest1);
+            test2Btn.onClick.AddListener(OnTest2);
+            test3Btn.onClick.AddListener(OnTest3);
         }
-
+        //AssetOperationHandle handle;
         public void OnTest()
         {
-            ModelManager.Instance.GetModel<MainModel>().EnterGame();
+            AssetManager.Instance.LoadSceneAsync("Login", UnityEngine.SceneManagement.LoadSceneMode.Single, true, AssetType.Scene);
+        }
+        public void OnTest1()
+        {
+            AssetManager.Instance.LoadSceneAsync("Game", UnityEngine.SceneManagement.LoadSceneMode.Single, true, AssetType.Scene);
+        }
+        public void OnTest2()
+        {
+            AssetManager.Instance.LoadAsset<GameObject>(out var handle, "Tips", AssetSuffix.Prefab, AssetType.Tool);
+        }
+        public void OnTest3()
+        {
+            //AssetManager.Instance.UnLoadAsset(handle);
+            //ModelManager.Instance.GetModel<MainModel>().EnterGame();
         }
     }
 }
