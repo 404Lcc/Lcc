6a33a17e352fc636b55105875a09057fb41d3977
Lccxy <1097304582@qq.com>
2023-08-20 20:20:24 +0800

    工具类

--- Assets/Framework/Runtime/Core/Expand/Transform/TransformExpand.cs
+++ Assets/Framework/Runtime/Core/Expand/Transform/TransformExpand.cs
@@ -4,6 +4,12 @@ namespace LccModel
 {
     public static class TransformExpand
     {
+        public static Vector2 sizeDelta(this RectTransform rectTransform)
+        {
+            Vector2 size = new Vector2(rectTransform.rect.size.x, rectTransform.rect.size.y);
+
+            return size;
+        }
         public static GameObject GetChildGameObject(this Transform transform, params string[] childs)
         {
             if (transform == null) return null;
