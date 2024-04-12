3d741880e94d123c525bd1d2b700c00dc61dbcc7
Lccxy <1097304582@qq.com>
2024-04-13 00:02:57 +0800

    editor代码修改

---
+++ Assets/Editor/Runtime/Core/FilterHotFixAssemblies/FilterHotFixAssemblies.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEditor.Build;
+using UnityEngine;
+
+namespace LccEditor
+{
+    public class FilterHotFixAssemblies : IFilterBuildAssemblies
+    {
+        public int callbackOrder => 0;
+        public string[] OnFilterAssemblies(BuildOptions buildOptions, string[] assemblies)
+        {
+            List<string> allHotUpdateDllNames = new List<string>() { "Unity.Hotfix.dll" };
+
+
+            // 将热更dll从打包列表中移除
+            return assemblies.Where(ass =>
+            {
+                string assName = Path.GetFileNameWithoutExtension(ass);
+                bool reserved = allHotUpdateDllNames.All(dll => !assName.Equals(dll, StringComparison.Ordinal));
+                if (!reserved)
+                {
+                    Debug.Log($"[FilterHotFixAssemblies] filter assembly:{assName}");
+                }
+                return reserved;
+            }).ToArray();
+        }
+    }
+}
