91cee89fe02f66e3d40add7d433b29b5f23c7665
Lccxy <1097304582@qq.com>
2024-04-12 23:17:25 +0800

    接入luban

---
+++ DataTables/gen.bat
@@ -0,0 +1,13 @@
+set WORKSPACE=..
+set LUBAN_DLL=%WORKSPACE%\Tools\Luban\Luban.dll
+set CONF_ROOT=.
+
+dotnet %LUBAN_DLL% ^
+    -t client ^
+    -c cs-simple-json ^
+    -d json ^
+    --conf %CONF_ROOT%\luban.conf ^
+    -x outputCodeDir=..\Assets/Hotfix/Runtime/Config ^
+    -x outputDataDir=..\Assets/Bundles/Config
+
+pause
