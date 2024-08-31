6bdb90b57addfe9e7e15c6fa3f60f11a1052f993
lccxy <1097304582@qq.com>
2024-08-31 23:32:42 +0800

    改名

--- Assets/Hotfix/Old/Runtime/GlobalConfig/SceneName/SceneName.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LccHotfix
-{
-    public class SceneName
-    {
-        public const string Login = "Login";
-        public const string Main = "Main";
-        public const string Game = "Game";
-    }
-}
