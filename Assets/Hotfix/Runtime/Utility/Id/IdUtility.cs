9f69e45fc5c93147a2c71fdd436dfe58e2db57fc
lccxy <1097304582@qq.com>
2024-09-28 22:55:07 +0800

    id

--- Assets/Hotfix/Runtime/Utility/Id/IdUtility.cs
+++ Assets/Hotfix/Runtime/Utility/Id/IdUtility.cs
@@ -1,44 +1,13 @@
-﻿using System.Collections.Generic;
+﻿using System.Threading;
 
 namespace LccHotfix
 {
     public static class IdUtility
     {
-        public static List<int> countList = new List<int>();
         public static int id;
         public static long GenerateId()
         {
-            if (countList.Count == 0)
-            {
-                id++;
-                return id;
-            }
-            for (int i = 1; i <= countList.Count; i++)
-            {
-                if (!countList.Contains(i))
-                {
-                    return i;
-                }
-            }
-            id++;
-            return id;
-        }
-        public static long GenerateInstanceId()
-        {
-            if (countList.Count == 0)
-            {
-                id++;
-                return id;
-            }
-            for (int i = 1; i <= countList.Count; i++)
-            {
-                if (!countList.Contains(i))
-                {
-                    return i;
-                }
-            }
-            id++;
-            return id;
+            return Interlocked.Increment(ref id);
         }
     }
 }
