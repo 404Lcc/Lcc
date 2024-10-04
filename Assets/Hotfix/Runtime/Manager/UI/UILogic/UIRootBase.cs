5a76a1937f6063b66c692633eb31938f56f72f13
lccxy <1097304582@qq.com>
2024-10-04 16:33:06 +0800

    UIRootBase

---
+++ Assets/Hotfix/Runtime/Manager/UI/UILogic/UIRootBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace LccHotfix
+{
+	public class UIRootBase : UILogicBase
+	{
+
+		public override bool OnChildClosed(WNode child)
+		{
+			return ((WRootNode)WNode).DefaultChildCheck();
+		}
+
+		public override bool OnEscape(ref EscapeType escapeType)
+		{
+			escapeType = EscapeType.AUTO_CLOSE;
+			return true;
+		}
+	}
+}
