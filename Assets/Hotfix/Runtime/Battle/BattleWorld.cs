e7475c2b3045ac5f794dab76408bd5dbd42ca284
lccxy <1097304582@qq.com>
2024-12-15 18:47:11 +0800

    battleworld

--- Assets/Hotfix/Runtime/Battle/BattleWorld.cs
+++ Assets/Hotfix/Runtime/Battle/BattleWorld.cs
@@ -6,12 +6,6 @@ namespace LccHotfix
 {
     public class BattleWorld : ECSWorld
     {
-        public const string ComID = "ComID";
-        public const string ComTag = "ComTag";
-        public const string ComFaction = "ComFaction";
-        public const string ComOwnerEntity = "ComOwnerEntity";
-        public const string ComUnityObjectRelated = "ComUnityObjectRelated";
-
         protected override void Setup()
         {
             base.Setup();
@@ -32,36 +26,11 @@ namespace LccHotfix
         {
             base.InitializeEntityIndices();
 
-            LogicContext.AddEntityIndex(new PrimaryEntityIndex<LogicEntity, long>(ComID, LogicContext.GetGroup(LogicMatcher.ComID), (e, c) => ((ComID)c).id));
-            LogicContext.AddEntityIndex(new EntityIndexEnum<LogicEntity, TagType>(ComTag, LogicContext.GetGroup(LogicMatcher.ComTag), (e, c) => ((ComTag)c).tag));
-            LogicContext.AddEntityIndex(new EntityIndex<LogicEntity, FactionType>(ComFaction, LogicContext.GetGroup(LogicMatcher.ComFaction), (e, c) => ((ComFaction)c).faction));
-            LogicContext.AddEntityIndex(new EntityIndex<LogicEntity, int>(ComOwnerEntity, LogicContext.GetGroup(LogicMatcher.ComOwnerEntity), (e, c) => ((ComOwnerEntity)c).ownerEntityID));
-            LogicContext.AddEntityIndex(new GroupEntityIndex<LogicEntity, int>(ComUnityObjectRelated, LogicContext.GetGroup(LogicMatcher.ComUnityObjectRelated), (e, c) => ((ComUnityObjectRelated)c).gameObjectInstanceID.Keys.ToArray()));
-        }
-
-        public LogicEntity GetEntityWithComID(long id)
-        {
-            return ((PrimaryEntityIndex<LogicEntity, long>)LogicContext.GetEntityIndex(ComID)).GetEntity(id);
-        }
-
-        public HashSet<LogicEntity> GetEntitiesWithComTag(TagType tag)
-        {
-            return ((EntityIndexEnum<LogicEntity, TagType>)LogicContext.GetEntityIndex(ComTag)).GetEntities(tag);
-        }
-
-        public HashSet<LogicEntity> GetEntitiesWithComFaction(FactionType faction)
-        {
-            return ((EntityIndex<LogicEntity, FactionType>)LogicContext.GetEntityIndex(ComFaction)).GetEntities(faction);
-        }
-
-        public HashSet<LogicEntity> GetEntitiesWithComOwnerEntity(int ownerEntityID)
-        {
-            return ((EntityIndex<LogicEntity, int>)LogicContext.GetEntityIndex(ComOwnerEntity)).GetEntities(ownerEntityID);
-        }
-
-        public LogicEntity GetEntitiesWithComUnityObjectRelated(int gameObjectInstanceID)
-        {
-            return ((GroupEntityIndex<LogicEntity, int>)LogicContext.GetEntityIndex(ComUnityObjectRelated)).GetEntity(gameObjectInstanceID);
+            LogicContext.AddEntityIndex(new PrimaryEntityIndex<LogicEntity, long>(typeof(ComID).Name, LogicContext.GetGroup(LogicMatcher.ComID), (e, c) => ((ComID)c).id));
+            LogicContext.AddEntityIndex(new EntityIndexEnum<LogicEntity, TagType>(typeof(ComTag).Name, LogicContext.GetGroup(LogicMatcher.ComTag), (e, c) => ((ComTag)c).tag));
+            LogicContext.AddEntityIndex(new EntityIndex<LogicEntity, FactionType>(typeof(ComFaction).Name, LogicContext.GetGroup(LogicMatcher.ComFaction), (e, c) => ((ComFaction)c).faction));
+            LogicContext.AddEntityIndex(new EntityIndex<LogicEntity, int>(typeof(ComOwnerEntity).Name, LogicContext.GetGroup(LogicMatcher.ComOwnerEntity), (e, c) => ((ComOwnerEntity)c).ownerEntityID));
+            LogicContext.AddEntityIndex(new GroupEntityIndex<LogicEntity, int>(typeof(ComUnityObjectRelated).Name, LogicContext.GetGroup(LogicMatcher.ComUnityObjectRelated), (e, c) => ((ComUnityObjectRelated)c).gameObjectInstanceID.Keys.ToArray()));
         }
     }
 }
