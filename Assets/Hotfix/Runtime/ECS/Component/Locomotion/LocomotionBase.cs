ad6934c6560445e47e8ce8c4b12e1ea3bcdebbfb
lccxy <1097304582@qq.com>
2025-05-15 00:44:04 +0800

    fix

--- Assets/Hotfix/Runtime/ECS/Component/Locomotion/LocomotionBase.cs
+++ Assets/Hotfix/Runtime/ECS/Component/Locomotion/LocomotionBase.cs
@@ -5,9 +5,9 @@ namespace LccHotfix
     public abstract class LocomotionBase : ILocomotion
     {
         public bool IsRuning { get; set; } = true;
-        public Vector3 CurPosition { get; set; }
-        public Quaternion CurRotation { get; set; }
-        public Vector3 CurScale { get; set; }
+        public Vector3 CurPosition { get; set; } = Vector3.zero;
+        public Quaternion CurRotation { get; set; } = Quaternion.identity;
+        public Vector3 CurScale { get; set; } = Vector3.one;
 
         public virtual void Update(float dt)
         {
