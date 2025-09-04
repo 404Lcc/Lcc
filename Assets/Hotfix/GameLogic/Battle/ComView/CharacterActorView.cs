53d44ee296e8afd63e4dc26e56ad39a4a0f622dc
lccxy <1097304582@qq.com>
2025-09-04 22:58:05 +0800

    增加CharacterActorView

---
+++ Assets/Hotfix/GameLogic/Battle/ComView/CharacterActorView.cs
@@ -0,0 +1,81 @@
+using LccModel;
+using UnityEngine;
+
+namespace LccHotfix
+{
+    public class CharacterActorView : ActorView
+    {
+        public Vector2 size;
+        public AABB bounds;
+        public string curAnimName;
+
+        public override void Init(GameObjectPoolObject poolObject)
+        {
+            base.Init(poolObject);
+
+            Main.GizmoService.AddGizmo(OnGizmos);
+        }
+
+        public override void Dispose()
+        {
+            base.Dispose();
+            Main.GizmoService.RemoveGizmo(OnGizmos);
+        }
+
+        public override void SyncTransform(Vector3 position, Quaternion rotation, Vector3 scale)
+        {
+            base.SyncTransform(position, rotation, scale);
+
+            UpdateBounds();
+        }
+
+        public void OnGizmos()
+        {
+            if (bounds == null)
+                return;
+
+            // 绘制障碍物
+            Gizmos.color = Color.red;
+            var center2D = (bounds.minPoint + bounds.maxPoint) * 0.5f;
+            var center = new Vector3(center2D.x, 0, center2D.y);
+            var size = new Vector3(bounds.Width(), 0, bounds.Height());
+            Gizmos.DrawCube(center, size);
+        }
+
+        public void SetSize(Vector2 size)
+        {
+            this.size = size;
+            this.bounds = new AABB(-size / 2, size / 2);
+            UpdateBounds();
+        }
+        
+        public void UpdateBounds()
+        {
+            var selfPos = new Vector2(GameObject.transform.position.x, GameObject.transform.position.z);
+            bounds.minPoint = selfPos - size / 2;
+            bounds.maxPoint = bounds.minPoint + size;
+        }
+
+        public float PlayAnim(string animName, bool isForce = false)
+        {
+            if (!isForce)
+            {
+                //相同动画不播放
+                var normalizedTime = Animator.GetCurrentAnimatorStateInfo(0).normalizedTime;
+                if (curAnimName == animName && normalizedTime <= 1)
+                {
+                    return GameUtility.GetAnimationClipLengthByNameMatched(Animator, animName);
+                }
+            }
+            else
+            {
+                // 强制停止当前动画
+                Animator.StopPlayback();
+            }
+
+            Animator.Play(animName, 0, 0);
+
+            return GameUtility.GetAnimationClipLengthByNameMatched(Animator, animName);
+        }
+    }
+}
