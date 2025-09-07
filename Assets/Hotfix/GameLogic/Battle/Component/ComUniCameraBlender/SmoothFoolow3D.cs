63f90bdec10edd4963f5da06013f1dfd040923f4
lccxy <1097304582@qq.com>
2025-09-07 21:03:13 +0800

    相机加震动

--- Assets/Hotfix/GameLogic/Battle/Component/ComUniCameraBlender/SmoothFoolow3D.cs
+++ Assets/Hotfix/GameLogic/Battle/Component/ComUniCameraBlender/SmoothFoolow3D.cs
@@ -5,8 +5,10 @@ public class SmoothFoolow3D : ICameraBlender
 {
     //x轴角度
     public float angleX;
+
     //y轴角度
     public float angleY = 45;
+
     //y轴距离
     public float distance = 20;
 
@@ -15,10 +17,17 @@ public class SmoothFoolow3D : ICameraBlender
 
     //相机的距离比例
     public float proportion = 1;
+
     //是否开启阻尼
     public bool isNeedDamping = true;
+
     //相机偏移
     public Vector3 offset = new Vector3(0, 5f, 0);
+
+    //震动相关
+    public float shakeIntensity;
+    public float shakeTimer;
+    public bool isShaking;
 
     public Transform Target { get; set; }
     public Camera Camera => Main.CameraService.CurrentCamera;
@@ -38,7 +47,11 @@ public class SmoothFoolow3D : ICameraBlender
             Quaternion localRotation;
             localRotation = Quaternion.Euler(angleY, angleX, 0);
 
-            Vector3 position = localRotation * new Vector3(0, 0, -distance * proportion) + Target.position + offset;
+            //应用震动偏移
+            Vector3 currentOffset = isShaking ? offset + Random.insideUnitSphere * shakeIntensity : offset;
+
+            Vector3 position = localRotation * new Vector3(0, 0, -distance * proportion) + Target.position + currentOffset;
+
             if (isNeedDamping)
             {
                 Camera.gameObject.transform.localRotation = Quaternion.Lerp(Camera.gameObject.transform.localRotation, localRotation, UnityEngine.Time.deltaTime * damping);
@@ -49,12 +62,31 @@ public class SmoothFoolow3D : ICameraBlender
                 Camera.gameObject.transform.localRotation = localRotation;
                 Camera.gameObject.transform.position = position;
             }
+
+            if (isShaking)
+            {
+                shakeTimer -= Time.deltaTime;
+                if (shakeTimer <= 0)
+                {
+                    isShaking = false;
+                }
+            }
         }
     }
-    
+
     public void ChangeTarget(Transform target)
     {
         Target = target;
+    }
+
+    public void ShakeCamera(float intensity = 0.5f, float duration = 0.5f)
+    {
+        if (intensity <= 0 || duration <= 0)
+            return;
+
+        shakeIntensity = intensity;
+        shakeTimer = duration;
+        isShaking = true;
     }
 
     public void Dispose()
