f2d506b0cb430f3ef2d32023d4f21be583ccc578
lccxy <1097304582@qq.com>
2025-09-15 22:24:21 +0800

    character支持2d和3d切换

--- Assets/Hotfix/GameLogic/Battle/Component/ComView/CharacterActorView.cs
+++ Assets/Hotfix/GameLogic/Battle/Component/ComView/CharacterActorView.cs
@@ -3,8 +3,15 @@ using UnityEngine;
 
 namespace LccHotfix
 {
+    public enum CharacterPlane
+    {
+        XY,
+        XZ,
+    }
+
     public class CharacterActorView : ActorView
     {
+        public CharacterPlane plane;
         public Vector2 size;
         public AABB bounds;
 
@@ -36,9 +43,26 @@ namespace LccHotfix
             // 绘制障碍物
             Gizmos.color = Color.red;
             var center2D = (bounds.minPoint + bounds.maxPoint) * 0.5f;
-            var center = new Vector3(center2D.x, 0, center2D.y);
-            var size = new Vector3(bounds.Width(), 0, bounds.Height());
+            var center = Vector3.zero;
+            var size = Vector3.zero;
+            switch (plane)
+            {
+                case CharacterPlane.XY:
+                    center = new Vector3(center2D.x, center2D.y, 0);
+                    size = new Vector3(bounds.Width(), bounds.Height(), 0);
+                    break;
+                case CharacterPlane.XZ:
+                    center = new Vector3(center2D.x, 0, center2D.y);
+                    size = new Vector3(bounds.Width(), 0, bounds.Height());
+                    break;
+            }
+
             Gizmos.DrawCube(center, size);
+        }
+
+        public virtual void SetPlane(CharacterPlane plane)
+        {
+            this.plane = plane;
         }
 
         public virtual void SetSize(Vector2 size)
@@ -47,17 +71,26 @@ namespace LccHotfix
             this.bounds = new AABB(-size / 2, size / 2);
             UpdateBounds();
         }
-        
+
         public virtual void UpdateBounds()
         {
-            var selfPos = new Vector2(GameObject.transform.position.x, GameObject.transform.position.z);
+            var selfPos = Vector2.zero;
+            switch (plane)
+            {
+                case CharacterPlane.XY:
+                    selfPos = new Vector2(GameObject.transform.position.x, GameObject.transform.position.y);
+                    break;
+                case CharacterPlane.XZ:
+                    selfPos = new Vector2(GameObject.transform.position.x, GameObject.transform.position.z);
+                    break;
+            }
+
             bounds.minPoint = selfPos - size / 2;
             bounds.maxPoint = bounds.minPoint + size;
         }
-        
+
         public virtual void ApplyDamage()
         {
-            
         }
 
         public float PlayAnim(string animName, bool isForce = false)
