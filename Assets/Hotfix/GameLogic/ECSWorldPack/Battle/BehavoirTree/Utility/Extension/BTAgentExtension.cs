ec59cb0439ebac4b1af0eeeae5c62a53cb8fea65
lccxy <1097304582@qq.com>
2025-10-03 13:18:51 +0800

    BehavoirTree

---
+++ Assets/Hotfix/GameLogic/ECSWorldPack/Battle/BehavoirTree/Utility/Extension/BTAgentExtension.cs
@@ -0,0 +1,31 @@
+namespace LccHotfix
+{
+    public static class BTAgentExtension
+    {
+        public static LogicEntity GetSelfEntity(this BTAgent agent)
+        {
+            return EntityUtility.GetEntity(agent.EntityId);
+        }
+        
+        public static LogicEntity GetOwnerPlayerHero(this BTAgent agent)
+        {
+            var entity = agent.GetSelfEntity();
+            if (entity == null)
+            {
+                return null;
+            }
+
+            if (entity.hasComHero)
+            {
+                return entity;
+            }
+
+            if (entity.hasComOwnerPlayer)
+            {
+                return EntityUtility.GetHero(entity.comOwnerPlayer.UID);
+            }
+
+            return null;
+        }
+    }
+}
