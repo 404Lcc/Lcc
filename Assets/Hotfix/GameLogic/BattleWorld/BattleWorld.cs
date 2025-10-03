28339002a61c03582d8d9df3c1afe414ee65b2f2
lccxy <1097304582@qq.com>
2025-10-03 13:51:18 +0800

    BattleWorld

---
+++ Assets/Hotfix/GameLogic/BattleWorld/BattleWorld.cs
@@ -0,0 +1,117 @@
+using System.Collections.Generic;
+using Entitas;
+using System.Linq;
+using LccModel;
+using UnityEngine;
+
+namespace LccHotfix
+{
+    public class BattleMode : GameModeBase
+    {
+        public ECSWorld world;
+        private StateMachine _fsm;
+
+        public override void Start()
+        {
+            base.Start();
+
+            _fsm = new StateMachine(this);
+            _fsm.AddNode<BattleStart>();
+            _fsm.Run<BattleStart>();
+        }
+
+        public override void Update()
+        {
+            base.Update();
+
+            if (_fsm == null)
+                return;
+            _fsm.Update();
+        }
+
+
+
+        public override void Release()
+        {
+            base.Release();
+
+            GameObject.Destroy(world.GetWorldData<BattleWorldData>().Map);
+
+            if (_fsm == null)
+                return;
+            _fsm = null;
+
+
+        }
+    }
+
+    public class BattleWorldData : IWorldData
+    {
+        public List<InGamePlayerData> PlayerList { get; private set; }
+        public GameObject Map { get; private set; }
+
+        public void Init(GameObject map, List<InGamePlayerData> playerList)
+        {
+            this.Map = map;
+            this.PlayerList = playerList;
+        }
+    }
+
+    public class BattleStart : IStateNode
+    {
+        public BattleMode mode;
+
+        public void OnCreate(StateMachine machine)
+        {
+            mode = machine.Owner as BattleMode;
+        }
+
+        public void OnEnter()
+        {
+        }
+
+        public void CreatePlayer()
+        {
+            foreach (var item in mode.world.GetWorldData<BattleWorldData>().PlayerList)
+            {
+                var obj = GameUtility.GetObj("Player");
+                var entity = EntityUtility.AddEntityWithID<CharacterActorView>(item.PlayerUID, obj);
+                var player = entity.GetView<CharacterActorView>(ViewCategory.Actor);
+                player.SetPlane(CharacterPlane.XZ);
+                player.SetSize(new Vector2(1, 1));
+
+                entity.AddComOwnerPlayer(item);
+                entity.AddComTag(TagType.Hero);
+                entity.AddComFaction(FactionType.Friend);
+                entity.AddComHP(int.MaxValue);
+                entity.AddComHero();
+                entity.AddComProperty();
+                entity.comProperty.Init(int.MaxValue, 100, 3);
+
+                HPView hpView = new HPView();
+                hpView.Init(entity, HeadbarType.NormalHP, 0);
+                entity.AddView(hpView, ViewCategory.HP);
+            }
+        }
+
+        public void OnUpdate()
+        {
+        }
+
+        public void OnExit()
+        {
+        }
+    }
+
+    public class BattleWorld : ECSWorld
+    {
+        public override void CreateSystems()
+        {
+            System = new ECSSystems();
+            System.Add(new SysInitializeBattle(this));
+            this.AddRender();
+            this.AddDeath();
+            this.AddBattle();
+        }
+    }
+}
---
+++ Assets/Hotfix/GameLogic/BattleWorld/SysInitializeBattle.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Entitas;
+using LccModel;
+using UnityEngine;
+
+namespace LccHotfix
+{
+    public class SysInitializeBattle : IInitializeSystem
+    {
+        private ECSWorld _world;
+
+        public SysInitializeBattle(ECSWorld world)
+        {
+            _world = world;
+        }
+
+        public void Initialize()
+        {
+            InitEntityIndex();
+            InitComponent();
+        }
+
+        private void InitEntityIndex()
+        {
+            _world.LogicContext.AddEntityIndex(new PrimaryEntityIndex<LogicEntity, long>(typeof(ComID).Name, _world.LogicContext.GetGroup(LogicMatcher.AllOf(LogicComponentsLookup.ComID)), (e, c) => ((ComID)c).id));
+            _world.LogicContext.AddEntityIndex(new EntityIndexEnum<LogicEntity, TagType>(typeof(ComTag).Name, _world.LogicContext.GetGroup(LogicMatcher.AllOf(LogicComponentsLookup.ComTag)), (e, c) => ((ComTag)c).tag));
+            _world.LogicContext.AddEntityIndex(new EntityIndex<LogicEntity, FactionType>(typeof(ComFaction).Name, _world.LogicContext.GetGroup(LogicMatcher.AllOf(LogicComponentsLookup.ComFaction)), (e, c) => ((ComFaction)c).faction));
+            _world.LogicContext.AddEntityIndex(new EntityIndex<LogicEntity, long>(typeof(ComOwnerEntity).Name, _world.LogicContext.GetGroup(LogicMatcher.AllOf(LogicComponentsLookup.ComOwnerEntity)), (e, c) => ((ComOwnerEntity)c).ownerEntityID));
+            _world.LogicContext.AddEntityIndex(new GroupEntityIndex<LogicEntity, int>(typeof(ComUnityObjectRelated).Name, _world.LogicContext.GetGroup(LogicMatcher.AllOf(LogicComponentsLookup.ComUnityObjectRelated)), (e, c) => ((ComUnityObjectRelated)c).gameObjectInstanceID.Keys.ToArray()));
+        }
+
+        private void InitComponent()
+        {
+            var mode = new BattleMode();
+            mode.world = _world;
+            _world.MetaContext.SetComUniGameMode(mode);
+            _world.MetaContext.SetComUniCameraBlender(new SmoothFoolow2D());
+            _world.MetaContext.SetComUniInGamePlayers(_world.GetWorldData<BattleWorldData>().PlayerList);
+            _world.MetaContext.SetComUniOrca();
+            _world.MetaContext.SetComUniDamage(new DamageBase());
+            _world.MetaContext.SetComUniFloatingText(new DNPFloatingText());
+        }
+    }
+}
