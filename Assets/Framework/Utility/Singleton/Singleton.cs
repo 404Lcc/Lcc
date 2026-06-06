21f58e80e95ae2ee829c64b4a3b71c5d32284702
Lcc <Lcc@qq.com>
2026-06-06 22:50:35 +0800

    fix

--- Assets/Framework/Utility/Singleton/Singleton.cs
+++ Assets/Framework/Utility/Singleton/Singleton.cs
@@ -2,93 +2,91 @@
 
 namespace LccModel
 {
-	public class Singleton<T> where T : Singleton<T>, new()
-	{
-		protected static T _instance = null;
+    public class Singleton<T> where T : Singleton<T>, new()
+    {
+        protected static T _instance = null;
 
-		public static T Instance
-		{
-			get
-			{
-				if (_instance == null)
-				{
-					_instance = new T();
-					_instance.OnInit();
-				}
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new T();
+                    _instance.OnInit();
+                }
 
-				return _instance;
-			}
-		}
+                return _instance;
+            }
+        }
 
-		public static void DestroyInstance()
-		{
-			if (_instance != null)
-			{
-				_instance.OnDestory();
-				_instance = null;
-			}
-		}
+        public static void DestroyInstance()
+        {
+            if (_instance != null)
+            {
+                _instance.OnDestroy();
+                _instance = null;
+            }
+        }
 
-		protected virtual void OnInit()
-		{
+        protected virtual void OnInit()
+        {
+        }
 
-		}
+        protected virtual void OnDestroy()
+        {
+        }
+    }
 
-		protected virtual void OnDestory()
-		{
+    public class SingletonMono<T> : MonoBehaviour where T : class, new()
+    {
+        protected static T _instance = null;
 
-		}
-	}
+        public static T Instance
+        {
+            get
+            {
+                if (!Application.isPlaying)
+                {
+                    return null;
+                }
 
-	public class SingletonMono<T> : MonoBehaviour where T : class, new()
-	{
-		protected static T _instance = null;
+                if (_instance == null)
+                {
+                    _instance = FindObjectOfType(typeof(T)) as T;
+                }
 
-		public static T Instance
-		{
-			get
-			{
-				if (!Application.isPlaying)
-				{
-					return null;
-				}
+                GameObject root = GameObject.Find("SingletonMono");
+                if (root == null)
+                {
+                    root = new GameObject("SingletonMono");
+                    DontDestroyOnLoad(root);
+                }
 
-				if (_instance == null)
-				{
-					_instance = FindObjectOfType(typeof(T)) as T;
-				}
+                if (_instance == null)
+                {
+                    GameObject go = new GameObject(typeof(T).ToString());
+                    _instance = go.AddComponent(typeof(T)) as T;
+                    go.transform.parent = root.transform;
+                }
 
-				GameObject root = GameObject.Find("SingletonMono");
-				if (root == null)
-				{
-					root = new GameObject("SingletonMono");
-					DontDestroyOnLoad(root);
-				}
+                return _instance;
+            }
+        }
 
-				if (_instance == null)
-				{
-					GameObject go = new GameObject(typeof(T).ToString());
-					_instance = go.AddComponent(typeof(T)) as T;
-					go.transform.parent = root.transform;
-				}
+        public static bool HaveInstance
+        {
+            get
+            {
+                GameObject root = GameObject.Find("SingletonMono");
+                if (root == null)
+                {
+                    return false;
+                }
 
-				return _instance;
-			}
-		}
-
-		public static bool HaveInstance
-		{
-			get
-			{
-				GameObject root = GameObject.Find("SingletonMono");
-				if (root == null)
-				{
-					return false;
-				}
-
-				Transform trans = root.transform.Find(typeof(T).ToString());
-				return trans != null;
-			}
-		}
-	}
+                Transform trans = root.transform.Find(typeof(T).ToString());
+                return trans != null;
+            }
+        }
+    }
 }
